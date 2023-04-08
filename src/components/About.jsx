6e16631c28668bb3a20e4f9a4c7cb638c25331f3
John Bob Ayensu <johnbayensu@gmail.com>
2023-04-08 16:23:12 +0000

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About(){
     return(
         <div className="about">
             <h3 className="about-title">About</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+            <p className="about-p">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         </div>
     )
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,19 +2,19 @@ import PassportPic from "../images/passport_pic.jpg";
 
 export default function Info(){
     return(
-        <div className="info">
+        <div className="container">
             <img className="info-image" src={PassportPic} alt="My passport Picture" />
             <h1 className="info-name">John Bob Ayensu</h1>
             <h3 className="info-job">Teacher</h3>
             <p className="info-web">AceTechSol.com</p>
-            <div className="row g-0">
-                <div className="col-sm-6 col-md-8">
+            <div className="row">
+                <div className="col">
                     <button type="button" className="btn btn-light">
                         <i className="fa fa-envelope"></i>
                         Email
                     </button>
                 </div>
-                <div className="col-sm-6 col-md-8">
+                <div className="col">
                     <button type="button" className="btn btn-primary">
                         <i className="fa fa-linkedin"></i>
                         LinkedIn
